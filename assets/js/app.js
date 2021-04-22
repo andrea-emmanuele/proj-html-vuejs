b7b1c31d0392673c839238835bc8d91943006f7b
andrea <git@andreaemmanuele.it>
2021-04-22 16:20:42 +0200

    jumbo completed

--- assets/js/app.js
+++ assets/js/app.js
@@ -7,19 +7,19 @@ new Vue({
                     items: [{
                         text: "Lorem ipsum dolor sit amet.",
                         class: "time",
-                        type: "text"
+                        type: "icon-text"
                     }]
                 },
                 innerRight: {
                     items: [{
                         text: "+1 333 3333-3333",
                         class: "phone",
-                        type: "text"
+                        type: "icon-text"
                     },
                     {
                         text: "hello@example.com",
                         class: "message",
-                        type: "text"
+                        type: "icon-text"
                     },
                     {
                         socials: [{
@@ -70,22 +70,45 @@ new Vue({
                     },
                     {
                         text: "get in touch",
-                        type: "button"
+                        type: "button",
+                        class: "primary"
                     }]
+            },
+            jumbo: {
+                items: [{
+                    text: "Lead~ <span class=\"wrapper\">Customer</span>",
+                    type: "h1"
+                },
+                {
+                    text: "Lorem ipsum dolor sit amet, consectetur adipisicing elit. Animi ea est eum incidunt necessitatibus odit officia praesentium quae, tempore vero.",
+                    type: "paragraph"
+                }],
+                actions: [{
+                    text: "get in touch",
+                    type: "button",
+                    class: "primary"
+                },
+                {
+                    text: "read more",
+                    type: "button",
+                    class: "secondary"
+                }]
             }
         },
         getItems (item) {
             switch (item.type) {
-                case "text": return `<i class="${item.class}"></i> 
+                case "icon-text": return `<i class="${item.class}"></i> 
                                      <p>${item.text}</p>`;
                 case "link": return `<a href="${item.href}">${item.text}</a>`;
-                case "button": return `<button>${item.text}</button>`;
+                case "button": return `<button class="${item.class}">${item.text}</button>`;
                 case "icons":
                     let content = "";
                     item.socials.forEach(social => {
                         content += `<a href="${social.href}"><i class="${social.prefix} ${social.class}"></i></a>`;
                     });
                     return content;
+                case "h1": return `<h1>${item.text}</h1>`;
+                case "paragraph": return `<p>${item.text}</p>`;
             }
         }
     }
